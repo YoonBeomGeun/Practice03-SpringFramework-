ce3dc2122dc0c8c058fa550ab001cd36c8d0e2e6
YoonBeomGeun <yunbumkun36@naver.com>
2023-11-11 01:22:10 +0900

    브라우저 > 컨트롤러 > views 기본 연습
    Practice(Servlet&JSP)에서 연습했던 파일을 이용하여 실습
    컨트롤러를 포함하여 로그인/로그아웃, 회원가입화면 구현
    (기본적인 흐름 확인)

---
+++ Pt02(SpringFramework)/java/com/multi/mvc01/MemberController.java
@@ -0,0 +1,40 @@
+package com.multi.mvc01;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	//버튼을 눌러서 요청 --> 버튼마다 함수 하나
+	//회원가입
+	@RequestMapping("insert")
+	public void insert(MemberDTO bag) {
+		//함수(bag) => 요청할 때 만든다.(프로토타입)
+		//함수에 bag 넣기 => 클라이언트로부터 전달된 값을 가방을 만들어서 넣기
+		System.out.println(bag);
+		
+		MemberDAO dao = new MemberDAO();
+	    int result = dao.insert(bag); //int(1/0)
+	    String resultText = "";
+	    if(result == 1){
+	    	resultText = "회원가입에 성공했습니다.";
+	    }else{
+	    	resultText = "회원가입에 실패했습니다.";
+	    }
+	    System.out.println(resultText);
+	}
+	
+	//로그인
+	@RequestMapping("login")
+	public void login(MemberDTO bag, HttpSession session) throws Exception {
+		MemberDAO dao = new MemberDAO();
+    	boolean result = dao.login(bag); //boolean(true/false)
+    	if(result){ //if(조건) {조건이 true이면 이 괄호를 실행해라!}
+    		//세션을 설정해주자.!!!
+    		session.setAttribute("id", bag.getId());
+    	}
+	}
+
+}
